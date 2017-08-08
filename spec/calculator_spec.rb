dc5b15ee0e4e4decfa720b04e2efa1b7ea5cef7e
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-08-08 16:28:20 +0200

    add context with invalid params to calculator tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,20 +2,41 @@ require 'spec_helper'
 require_relative '../lib/calculator'
 
 describe Calculator do
-  let(:first_number) { 2 }
-  let(:second_number) { 3 }
 
-  context '#multiply' do
-    subject { described_class.new(first_number, second_number) }
+  subject { described_class.new(first_number, second_number) }
+  context 'with valid params' do
+    let(:first_number) { 2 }
+    let(:second_number) { 3 }
+    describe '#multiply' do
+      it 'should return proper number' do
+        expect(subject.multiply).to eq 6
+      end
+    end
 
-    it 'should return proper number' do
-      expect(subject.multiply).to eq 6
+    describe '.add' do
+      it 'should return proper number' do
+        expect(described_class.add(first_number, second_number)).to eq 5
+      end
     end
   end
 
-  context '.add' do
-    it 'should return proper number' do
-      expect(described_class.add(first_number, second_number)).to eq 5
+  context 'with invalid params' do
+    let(:first_number) { 'two' }
+    let(:second_number) { 'three' }
+    describe '#multiply' do
+      it 'should rise TypeError when string given' do
+        expect{subject.multiply}.to raise_error(TypeError)
+      end
+    end
+
+    describe '.add' do
+      it 'should return proper number' do
+        expect(described_class.add(first_number, second_number)).not_to eq 5
+      end
+
+      it 'should rise ArgumentError when no params given' do
+        expect{described_class.add}.to raise_error(ArgumentError)
+      end
     end
   end
 end
